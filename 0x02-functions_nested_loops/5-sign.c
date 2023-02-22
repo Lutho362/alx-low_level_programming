45da12cca27450b8428c59bda0bb8d02e75ddc2b
lutho362 <lutho470@gmail.com>
2023-02-22 03:16:44 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number whether it is negative or positive
  * @n: is the number tha is gonna be checked
- * Return: return 1 and + if the number is positive, 0 and 0 if its 0, otherwyze -1 and - if it is negative
+ * Return: return 1 and + or 0 and 0, or -1 and -
  */
 
 int print_sign(int n)
