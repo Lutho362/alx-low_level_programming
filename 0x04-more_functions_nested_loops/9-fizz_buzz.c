b908ec79e6c6293a7635c42590f6b618df874d10
lutho362 <lutho470@gmail.com>
2023-02-24 04:07:54 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "main.h"
+
 #include <stdio.h>
 
 /**
@@ -12,7 +12,7 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
+		if ((x % 3 = 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
