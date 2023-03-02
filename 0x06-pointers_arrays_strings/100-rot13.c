38447c959d11a48efe1abbacd138c9ec02acc4a1
lutho362 <lutho470@gmail.com>
2023-03-02 03:13:56 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,19 +9,20 @@
 
 char *rot13(char *s)
 {
-       int i, j;
-       char data1[] = "ABCDEFGHIJKLMNOPQRSTUYZabcdeghijklmnopqrstuvwxyz";
-       char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-       for(i  =  0; s[i] != '\0'; i++)
-       {
-               for (j = 0; j < 52; j++)
-               {
-                       if (s[i] == data1[j])
-                       {
-                               s[i] = datarot[j];
-                               break;
-                       }
-               }
-       }
-       return(s);
+	int i, j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUYZabcdeghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i  =  0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
